fc3fc299559605b69234665a2f73375e0eed8b7d
Sggg5 <1144344516@qq.com>
2026-09-14 15:46:47 +0800

    feat: add Atelier Time watch builder

---
+++ src/components/MovementTray.vue
@@ -0,0 +1,8 @@
+<template>
+  <aside class="movement-tray" aria-label="Movement selection">
+    <button v-for="movement in movements" :key="movement.id" class="movement" :class="{ selected: movement.id === store.selectedMovement }" @click="store.selectMovement(movement.id)">
+      <span class="movement-disc" :class="movement.tone"><i /><b /><em /></span><span><strong>{{ movement.code }}</strong><small>{{ movement.spec }}</small></span>
+    </button>
+  </aside>
+</template>
+<script setup>import { movements } from '../data/watchParts'; import { useWatchBuilderStore } from '../stores/watchBuilder'; const store=useWatchBuilderStore()</script>
