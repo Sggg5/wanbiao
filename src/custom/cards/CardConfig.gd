c9ddc2ab706fcf0d6ff34a78da79359d21245e8b
Sggg5 <1144344516@qq.com>
2026-09-14 15:46:58 +0800

    feat: add Atelier Time watch builder

---
+++ src/custom/cards/CardConfig.gd
@@ -0,0 +1,7 @@
+class_name CardConfig
+extends RefCounted
+
+const PROPERTIES_STRINGS := ["Type", "Abilities"]
+const PROPERTIES_NUMBERS := ["Cost"]
+const PROPERTIES_ARRAYS := ["Tags"]
+const SCENE_PROPERTY := "Type"
