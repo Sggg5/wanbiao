741c02f4a14ab732e6b785cb2efa556b69e948a1
Sggg5 <1144344516@qq.com>
2026-09-14 15:46:55 +0800

    feat: add Atelier Time watch builder

---
+++ src/components/watch/WatchLayer.vue
@@ -0,0 +1,2 @@
+<template><Transition name="layer-swap" mode="out-in"><div :key="keyValue" class="watch-layer" :class="[`layer-${layer}`,keyValue]"><img v-if="src && layer !== 'dial'" :src="src" alt="" /><span v-if="layer === 'dial'" class="dial-surface" :style="{background: color}" /></div></Transition></template>
+<script setup>defineProps({ layer:String,src:String,color:{type:String,default:'transparent'},keyValue:String })</script>
