41cfdc56932fef032661528f4c1cf2f5a41dade7
Sggg5 <1144344516@qq.com>
2026-09-14 15:49:23 +0800

    chore: remove unrelated workspace files

--- src/custom/cards/CardConfig.gd
+++
@@ -1,7 +0,0 @@
-class_name CardConfig
-extends RefCounted
-
-const PROPERTIES_STRINGS := ["Type", "Abilities"]
-const PROPERTIES_NUMBERS := ["Cost"]
-const PROPERTIES_ARRAYS := ["Tags"]
-const SCENE_PROPERTY := "Type"
