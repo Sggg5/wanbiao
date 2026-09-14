381b178e5f77caea4732344fb42101d1e5574b4f
Sggg5 <1144344516@qq.com>
2026-09-14 15:46:45 +0800

    feat: add Atelier Time watch builder

---
+++ src/components/CompatibilityConfirm.vue
@@ -0,0 +1,2 @@
+<template><div v-if="store.pendingMovement" class="compatibility-confirm"><div><p>CHANGE MOVEMENT?</p><span>This change will reset:</span><strong>{{ store.pendingMovement.invalid.join(' · ').toUpperCase() }}</strong><footer><button @click="store.cancelMovement">CANCEL</button><button @click="store.confirmMovement">CONTINUE</button></footer></div></div></template>
+<script setup>import { useWatchBuilderStore } from '../stores/watchBuilder'; const store=useWatchBuilderStore()</script>
