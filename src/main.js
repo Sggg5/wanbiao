7b9f7ddd5d2dc8748d73b01fb406fb8ad0eb789b
Sggg5 <1144344516@qq.com>
2026-09-16 15:52:48 +0800

    style: load phase4 layer overrides

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './styles.css'
 import './stability.css'
+import './phase4.css'
 
 createApp(App).use(createPinia()).mount('#app')
