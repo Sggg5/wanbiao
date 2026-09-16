1e08035d88225a6180a3aa15154d7d3af0d9ba94
Sggg5 <1144344516@qq.com>
2026-09-16 13:06:18 +0800

    fix: version serialized watch builds

--- src/utils/buildState.js
+++ src/utils/buildState.js
@@ -1,3 +1,23 @@
-export const serializeBuild = (build) => ({m:build.movement,c:build.case,d:build.dial,h:build.hands,s:build.strap,b:build.caseback})
+export const BUILD_STATE_VERSION = '1'
+
+export const serializeBuild = (build) => ({
+  v: BUILD_STATE_VERSION,
+  m: build.movement,
+  c: build.case,
+  d: build.dial,
+  h: build.hands,
+  s: build.strap,
+  b: build.caseback,
+})
+
 const read = (value, key) => typeof value?.get === 'function' ? value.get(key) : value?.[key]
-export const deserializeBuild = (value={}) => ({movement:read(value,'m'),case:read(value,'c'),dial:read(value,'d'),hands:read(value,'h'),strap:read(value,'s'),caseback:read(value,'b')})
+
+export const deserializeBuild = (value = {}) => ({
+  version: read(value, 'v'),
+  movement: read(value, 'm'),
+  case: read(value, 'c'),
+  dial: read(value, 'd'),
+  hands: read(value, 'h'),
+  strap: read(value, 's'),
+  caseback: read(value, 'b'),
+})
